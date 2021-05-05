79d367d1fcf4efcc606f8881fe6d4736c0478bcd
Ekta Arora <ekta.arora464@gmail.com>
2021-05-05 19:50:02 -0400

    constructor vs autowired dependency injection

--- twitter-to-kafka-service/src/main/java/com/microservice/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservice/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,11 +1,30 @@
 package com.microservice.demo.twitter.to.kafka.service;
 
+import com.microservice.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+
+    //Prefer constructor injection over field injection using Autowired as it favors immutability using final keyword
+    //and forces object creation with the injected object & no reflection & No @Autowired annotation is required.
+    //Reflection resolution slows down the application as the dependencies are resolved at runtime
+    /*private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData) {
+        this.twitterToKafkaServiceConfigData = configData;
+    }*/
+
+    @Autowired
+    private TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
 
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
@@ -22,6 +41,8 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception{
-        System.out.println("App starts...");
+        LOG.info("App starts...");
+        LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
+        LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
     }
 }
